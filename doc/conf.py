1c2010d2027a97788506c23f64df48ac5ce2dfc8
Emmanuel Cieren <129041266+ecieren@users.noreply.github.com>
2024-02-21 10:50:08 +0100

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -16,7 +16,7 @@ sys.path.insert(0, os.path.abspath('../'))
 # -- Project information -----------------------------------------------------
 
 project = 'slenderpy'
-copyright = '2024, Eurobios Mews Labs'
+copyright = '2024, RTE & Eurobios Mews Labs'
 author = 'Eurobios Mews Labs'
 
 # The full version, including alpha/beta/rc tags
