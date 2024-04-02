c7402b6797d8bbe3c69ee746262cf4d493180f52
ecieren <emmanuel.cieren@mews-labs.com>
2024-04-02 15:13:44 +0200

    added nleq to examples

--- examples/cable_static.py
+++ examples/cable_static.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from slenderpy.future.cable.static import parabolic
+from slenderpy.future.cable.static import nleq
 
 
 def parabola_plot():
@@ -13,9 +14,9 @@ def parabola_plot():
     tension = 0.012 * 1.85E+05
     sld = 10.
     linm = 1.57
-    # axs = 3.65E+07
+    axs = 3.65E+07
 
-    # compute cable stuff
+    # [parabolic] compute cable stuff
 
     # static shape
     xp = np.linspace(0, lspan, 1001)
@@ -32,13 +33,31 @@ def parabola_plot():
     print(f"[parabola] cable length {length:.6f}")
     print(f"[parabola] sag {sag:.6f}")
 
+    # [nleq] compute cable stuff
+
+    # solve equilibrium, get cable length and static shape
+    length_, lve = nleq.solve(lspan, tension, sld, linm, axs)
+    xn, yn = nleq.shape(1001, lspan, tension, sld, linm, axs, lcab=length_, lve=lve)
+    # cable sag
+    sag_ = nleq.sag(lspan, tension, sld, linm, axs, lcab=length_, lve=lve)
+    # sag points
+    lt = nleq.argsag(lspan, tension, sld, linm, axs, lcab=length_, lve=lve)
+    xt, yt = nleq.shape(lt, lspan, tension, sld, linm, axs, lcab=length_, lve=lve)
+
+    # print
+    print(f"[nleq] cable length {length_:.6f}")
+    print(f"[nleq] sag {sag_:.6f}")
+
     # plot
     plt.figure()
-    plt.plot(xp, xp * sld / lspan, ls='--', c='gray')
+    plt.plot([0., lspan], [0, sld], ls='--', c='gray')
     plt.plot(xp, yp, label='Parabola', c='C0')
     plt.plot([xs, xs], [ys, xs * sld / lspan], ls='--', c='C0')
-
+    plt.plot(xn, yn, label='NL eq.', c='C1')
+    plt.plot([xt, xt], [yt, xt * sld / lspan], ls='--', c='C1')
     plt.legend()
+    plt.xlabel("$x$")
+    plt.ylabel("$y$")
     plt.grid(True)
 
     return
